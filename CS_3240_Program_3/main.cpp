2665db3fc19f689869d88e732aaffdd1fc444bec
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-03-21 20:49:40 -0600

    fixed

--- CS_3240_Program_3/main.cpp
+++ CS_3240_Program_3/main.cpp
@@ -13,27 +13,39 @@
  X -> aYb|bYa
  Y -> aY|bY|lambda
 */
+
 #include <iostream>
 #include <deque>
 #include <ctime>
+#include <map>
+#include <utility>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
-enum State {S, aSa, bSb, X, aYb, bYa, aY, bY, Y};
-deque<char> mydeque;
+enum State {S, aSa, bSb, X, aYb, bYa, aY, bY, Y, A, B, Lambda};
+vector<char> vec;
 void cfg();
+void cfg_129();
 
 int main(int argc, const char * argv[]) {
     srand(time(NULL));              /* reset random numbers */
-    for (int i = 0; i < 5; i++)     /* 5 random strings from the cfg */
+    
+    cout << vec.size() << endl;
+    for (int i = 0; i < 5; i++)      /* 5 random strings from the cfg */
         cfg();
+
+    cfg_129();
+    
+    cout << endl;
     return 0;
 }
 
 /* context-free grammar function */
 void cfg(){
     State state = S;
-    mydeque.clear();
+    vec.clear();
     int ran;
     do{
         switch (state) {
@@ -48,8 +60,10 @@ void cfg(){
                 break;
             case aSa:
                 ran = rand() % 3;
-                mydeque.push_front('a');
-                mydeque.push_back('a');
+                vec.push_back('a');
+                vec.push_back('a');
+                cout << vec.back();
+                vec.pop_back();
                 if(ran == 0)
                     state = aSa;
                 else if(ran == 1)
@@ -59,8 +73,10 @@ void cfg(){
                 break;
             case bSb:
                 ran = rand() % 3;
-                mydeque.push_front('b');
-                mydeque.push_back('b');
+                vec.push_back('b');
+                vec.push_back('b');
+                cout << vec.back();
+                vec.pop_back();
                 if(ran == 0)
                     state = aSa;
                 else if(ran == 1)
@@ -76,8 +92,10 @@ void cfg(){
                     state = bYa;
             case aYb:
                 ran = rand() % 3;
-                mydeque.push_front('a');
-                mydeque.push_back('b');
+                vec.push_back('a');
+                vec.push_back('b');
+                cout << vec.back();
+                vec.pop_back();
                 if(ran == 0)
                     state = aY;
                 else if(ran == 1)
@@ -87,7 +105,9 @@ void cfg(){
                 break;
             case aY:
                 ran = rand() % 3;
-                mydeque.push_front('a');
+                vec.push_back('a');
+                cout << vec.back();
+                vec.pop_back();
                 if(ran == 0)
                     state = aY;
                 else if(ran == 1)
@@ -97,7 +117,9 @@ void cfg(){
                 break;
             case bY:
                 ran = rand() % 3;
-                mydeque.push_front('b');
+                vec.push_back('b');
+                cout << vec.back();
+                vec.pop_back();
                 if(ran == 0)
                     state = aY;
                 else if(ran == 1)
@@ -110,7 +132,45 @@ void cfg(){
         }
     }while(state != Y);
     
-    for(int i = 0; i <= mydeque.size(); i++)
-        cout << mydeque[i];
+    for(int i = 0; i <= vec.size(); i++){
+        cout << vec[i];
+        vec.pop_back();
+    }
     cout << "\n";
+}
+
+/* cfg on page 129 */
+void cfg_129(){
+    State state = A;
+    vec.clear();
+    int ran;
+    do{
+        switch (state) {
+            case A:
+                ran = rand() % 2;
+                vec.push_back('a');
+                vec.push_back('a');
+                for (int i = 0; i <= vec.size(); i++) {
+                    cout << vec.back();
+                    vec.pop_back();
+                }
+                if(ran == 0)
+                    state = A;
+                else
+                    state = B;
+                    break;
+            case B:
+                ran = rand() % 2;
+                vec.push_back('b');
+                cout << vec.back();
+                vec.pop_back();
+                if(ran == 0)
+                    state = B;
+                else
+                    state = Lambda;
+                break;
+            default:
+                break;
+        }
+    }while(state != Lambda);
 }
